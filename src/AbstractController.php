9b9c6bd806a641aeea2beba9a5848eadaf6969a1
Eko Kurniawan <setiawanegho@gmail.com>
2015-07-16 14:31:07 +0700

    Add AbstractController

---
+++ src/AbstractController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace moe;
+
+use tools\Template;
+
+class AbstractController
+{
+    /**
+     * Render view or array (as json)
+     * @param  mixed  $object  String view or array
+     * @param  boolean $return wether to return content or not
+     * @return string or nothing
+     */
+    public function render($object, $return = false)
+    {
+        $content = is_array($object)?json_encode($object):
+            Template::instance()->render($object);
+        if ($return)
+            return $content;
+        echo $content;
+    }
+}
