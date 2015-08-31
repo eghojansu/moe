e2229e0339ab5eb6042c284577083d315ee340dd
Eko Kurniawan <setiawanegho@gmail.com>
2015-08-31 11:14:40 +0700

    REL-0.5.0

--- src/Base.php
+++ src/Base.php
@@ -12,7 +12,7 @@ final class Base extends Prefab implements ArrayAccess {
 	//@{ Framework details
 	const
 		PACKAGE='Moe Framework',
-		VERSION='0.4.0';
+		VERSION='0.5.0';
 	//@}
 
 	//@{ HTTP status codes (RFC 2616)
